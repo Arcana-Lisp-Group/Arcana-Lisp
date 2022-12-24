c6e6cdee93c1ec673571cc626830972e9ce5a44b
NeilKleistGao <neilkleist@gmail.com>
2022-12-24 12:37:46 +0800

    Add lexer test

---
+++ src/test/scala/arcana/empress/LexerTest.scala
@@ -0,0 +1,30 @@
+package arcana.empress
+
+import org.scalatest.funsuite.AnyFunSuite
+import java.io.{BufferedReader, FileReader, BufferedWriter, FileWriter}
+import arcana.empress.{Lexer, TokenPrettyPrinter, LexerOptions}
+
+class LexerTest extends AnyFunSuite {
+  import LexerTest._
+
+  testList.foreach((name) => test(name) {
+    val reader = new BufferedReader(new FileReader(s"src/test/arc/$name"))
+    val code = readFromFile(reader)
+
+    val writer = new BufferedWriter(new FileWriter(s"src/test/tokens/$name.tokens"))
+    writer.write(TokenPrettyPrinter(Lexer(code, LexerOptions(None, true)).tokens))
+    writer.close()
+  })
+}
+
+object LexerTest {
+  private val testList = List(
+    "hello.arc"
+  )
+
+  private def readFromFile(reader: BufferedReader, prev: String = ""): String = {
+    val line = reader.readLine()
+    if (line == null) prev
+    else readFromFile(reader, s"$prev$line\n")
+  }
+}
