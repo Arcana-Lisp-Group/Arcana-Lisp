2bc2bbd7e33285211011edfc09ae6757f157b589
NeilKleistGao <neilkleist@gmail.com>
2023-01-31 21:42:09 +0800

    Add definition parsing and remove list parsing parameter

--- src/main/scala/arcana/emperor/Parser.scala
+++ src/main/scala/arcana/emperor/Parser.scala
@@ -9,7 +9,7 @@ class Parser(tokens: List[(Token, Option[Location])], options: ParserOptions) {
     tokens: List[(Token, Option[Location])],
     previous: List[ASTNode with Expression]
   ): List[ASTNode with Expression] = tokens match {
-    case (PunctuationToken('('), _) :: tail => parseList(tail, PunctuationToken(')')) match {
+    case (PunctuationToken('('), _) :: tail => parseList(tail) match {
       case (res, rest) => parse(rest, previous :+ res)
     }
     case (PunctuationToken('['), pos) :: tail =>
@@ -27,15 +27,14 @@ class Parser(tokens: List[(Token, Option[Location])], options: ParserOptions) {
   }
 
   private def parseList(
-    tokens: List[(Token, Option[Location])],
-    expectedEndToken: PunctuationToken
+    tokens: List[(Token, Option[Location])]
   ): (ASTNode with Expression, List[(Token, Option[Location])]) = tokens match {
-    case (KeywordToken("def"), pos) :: tail => parseDefinition(tail)
+    case (KeywordToken("def"), pos) :: tail => parseDefinition(tail, pos)
     case (KeywordToken("fun"), pos) :: tail => parseAbstraction(tail)
     case (KeywordToken("let"), pos) :: tail => parseLetBinding(tail)
     case (KeywordToken("if"), pos) :: tail => parseCondition(tail)
-    case (PunctuationToken('('), _) :: tail => parseList(tail, PunctuationToken(')')) match {
-      case (res, (PunctuationToken(expectedEndToken.punc), _) :: rest) => (res, rest)
+    case (PunctuationToken('('), _) :: tail => parseList(tail) match {
+      case (res, (PunctuationToken(')'), _) :: rest) => (res, rest)
       case (_, (token, pos) :: rest) => (Error("need ')'.")(pos), (token, pos) +: rest)
       case (_, Nil) => (Error("unexpected eof.")(None), Nil)
     }
@@ -44,7 +43,7 @@ class Parser(tokens: List[(Token, Option[Location])], options: ParserOptions) {
     case (PunctuationToken('{'), pos) :: tail =>
       (Error("unexpected '{'.")(pos), tail.dropWhile(!_.equals(PunctuationToken('}'))))
     case (token: IdentifierToken, pos) :: tail => parseEvaluation(tail)
-    case (token: LiteralToken, pos) :: expectedEndToken :: tail =>
+    case (token: LiteralToken, pos) :: (PunctuationToken(')'), _) :: tail =>
       (SingleExpression(Right(token))(pos), tail)
     case (token: LiteralToken, pos) :: tail =>
       (Error("need close bracket.")(pos), tail)
@@ -54,8 +53,34 @@ class Parser(tokens: List[(Token, Option[Location])], options: ParserOptions) {
     case Nil => (Error("unexpected eof.")(None), Nil)
   }
 
+  private def parseDefinition(tokens: List[(Token, Option[Location])], startPos: Option[Location]) = tokens match {
+    case (IdentifierToken(name), _) :: rest => {
+      val (ty, expHead) = rest match {
+        case (IdentifierToken(typeName), pos) :: rest if (typeName.startsWith(":")) =>
+          (TypeAnnotation(typeName.substring(1))(pos), rest)
+        case _ => (InferNeededType(), rest)
+      }
+
+      val (exp, tail) = expHead match {
+        case (PunctuationToken('('), _) :: tail => parseList(tail)
+        case (token: IdentifierToken, pos) :: tail => (SingleExpression(Left(token))(pos), tail)
+        case (token: LiteralToken, pos) :: tail => (SingleExpression(Right(token))(pos), tail)
+        case (ErrorToken(info), pos) :: tail => (Error(info)(pos), tail)
+        case (_, pos) :: tail => (Error("expect expression to be binded.")(pos), tail)
+        case Nil => (Error("unexpected eof.")(None), Nil)
+      }
+
+      val fullPos = (startPos, tail) match {
+        case (Some(startPos), (_, Some(endPos)) :: _) => Some(Location(startPos.start, endPos.end, startPos.filename))
+        case _ => None
+      }
+      (Definition(name, ty, exp)(fullPos), tail)
+    }
+    case (_, pos) :: tail => (Error("need variable name after 'def'.")(pos), tail)
+    case Nil => (Error("unexpected eof.")(None), Nil)
+  }
+
   // TODO:
-  private def parseDefinition(tokens: List[(Token, Option[Location])]) = ???
   private def parseAbstraction(tokens: List[(Token, Option[Location])]) = ???
   private def parseLetBinding(tokens: List[(Token, Option[Location])]) = ???
   private def parseCondition(tokens: List[(Token, Option[Location])]) = ???
